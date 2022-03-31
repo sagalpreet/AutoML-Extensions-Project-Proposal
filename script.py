17ad59bb5fe473c0de69be955b65d27e6305d7d3
Sagalpreet Singh <52735377+sagalpreet@users.noreply.github.com>
2022-04-01 02:50:27 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,27 @@
+import os
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+
+FILE = f'{DIR}/test/unit_test/automl_test.cc'
+
+content = None
+
+with open(FILE, 'r') as f:
+    content = f.read().split('  const size_t deterministic_champ_switch = 161;')
+
+content1 = content[0]
+text = '  const size_t deterministic_champ_switch = '
+content2 = content[1]
+
+x = '/media/sagalpreet/Data/Sagal/RLOS/vowpal_wabbit/build/test/unit_test'
+
+
+for i in range(1332):
+    with open(FILE, 'w') as f:
+        s = text + str(i) + ';'
+        f.write(content1+s+content2)
+    os.system(f'make -C {x} > /dev/null')
+    val = os.system(f'{x}/vw-unit-test.out -t automl_first_champ_switch > /dev/null 2> /dev/null')
+    print(i, val)
+    if (val == 0):
+        break
